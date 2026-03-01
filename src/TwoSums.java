0a3e481ae79bc11a11751b6348f920daacab1ccb
umarfarooqazmi <umarfarooqazmi22@gmail.com>
2026-03-01 16:02:44 +0530

    Session 6

--- src/TwoSums.java
+++ src/TwoSums.java
@@ -12,3 +12,16 @@ class Solution {
         return new int[]{};
     }
 }
+
+// Hashmap version for future reference:
+//public int[] twoSum(int[] nums, int target) {
+//    HashMap<Integer, Integer> map = new HashMap<>();
+//    for (int i = 0; i < nums.length; i++) {
+//        int complement = target - nums[i];
+//        if (map.containsKey(complement)) {
+//            return new int[]{map.get(complement), i};
+//        }
+//        map.put(nums[i], i);
+//    }
+//    return new int[]{};
+//}
